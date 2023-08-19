43b22c976276388abf751ff6505ce6a7b7779c84
wallleap <15579576761@163.com>
2023-08-19 23:41:22 +0800

    🎉 init: create project

---
+++ docs/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Header from './components/Header.vue'
+</script>
+
+<template>
+  <Header />
+  <main>
+    <router-view />
+  </main>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ docs/components/Header.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <nav>
+    <a href="/">
+      <img class="logo" src="/vite.svg" alt="Vue logo" />
+    </a>
+    <ul>
+      <li><a href="/docs/introduction">Docs</a></li>
+      <li><a href="/api">API</a></li>
+    </ul>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+
+</style>

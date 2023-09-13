9e820389b8eb3eb3b8090c9cb0d17920feffc445
wallleap <15579576761@163.com>
2023-09-13 22:10:52 +0800

    🔧 build: config doc page

---
+++ vite.config.page.ts
@@ -0,0 +1,26 @@
+
+/** @type {import('vite').UserConfig} */
+import { defineConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
+import Markdown from 'vite-plugin-md'
+import prism from 'markdown-it-prism'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [
+    Vue({
+      include: [/\.vue$/, /\.md$/],
+    }),
+    Markdown({
+      markdownItOptions: {
+        html: true,
+        linkify: true,
+        typographer: true,
+      },
+      markdownItSetup(md) {
+        md.use(prism)
+      },
+    }),
+  ],
+})
